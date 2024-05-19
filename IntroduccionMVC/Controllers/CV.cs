8c22e2a0fa41ec12b91841f0f75e8cbcb40d30e5
Chistoper Ramirez <85514595+IvanRS9@users.noreply.github.com>
2024-05-19 13:44:00 -0600

    Estructura de CV con links, falta front BS o TW

---
+++ IntroduccionMVC/Controllers/CV.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace IntroduccionMVC.Controllers
+{
+	public class CV : Controller
+	{
+		public IActionResult Index()
+		{
+			return View();
+		}
+
+		public IActionResult Contact()
+		{
+			return View();
+		}
+
+		public IActionResult Academy()
+		{
+			return View();
+		}
+
+		public IActionResult Experience()
+		{
+			return View();
+		}
+	}
+}
